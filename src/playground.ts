b2b8e89d9a7850e41a3333ce959cbbaf042da3ff
Aryanpatel2001 <patelaryan8044@gmail.com>
2024-11-17 14:59:57 +0530

    initial commit for clerk

---
+++ src/playground.ts
@@ -0,0 +1,11 @@
+import { db } from "./server/db";
+
+await db.user.create({
+  data: {
+    emailAddress: "test12@gmail.com",
+    firstName: "elliottee",
+    lastName: "chong2",
+  },
+});
+
+console.log("done");
